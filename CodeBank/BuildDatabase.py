083c02000bc535d98e06ff79c956548142bcaee5
Michael Wegner <miwe7500@colorado.edu>
2019-04-25 20:06:53 -0600

    Created Antoku_Docker. This folder contains the working components of the app along with the docker image.

--- CodeBank/BuildDatabase.py
+++ CodeBank/BuildDatabase.py
@@ -4,9 +4,11 @@ from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults,GetUpdatedProp
 zws_id= "X1-ZWz1h11b99c5qj_7kib9"
 
 f_name = open("CompleteAddress.txt", "r")
-data_base= open("AdressDatabase.txt","+w")
+data_base= open("lat_test.txt","+w")
 write_out="street_address | zipcode | zillow_id | Zestimate | Home_type | num_bath | num_bed | home_size"
 data_base.write(write_out)
+
+f_name=["2893 Springdale Ln, Boulder, CO 80303, USA","3000 Broadway #2, Boulder, CO 80304, USA","1623 19th St, Boulder, CO 80302, USA"]
 
 for line in f_name:
 	line=line.rstrip("\n")
@@ -26,11 +28,13 @@ for line in f_name:
 		Home_type=result.home_type
 		Zestimate=result.zestimate_amount
 		zillow_id=result.zillow_id
+		lat=result.latitude
+		long_res=result.longitude
 
 		num_bath=str(result.bathrooms)
 		num_bed=str(result.bedrooms)
 		home_size=str(result.home_size)
-		write_out=street_address+'|'+zipcode+"|"+zillow_id+"|"+Zestimate+"|"+Home_type+"|"+num_bath+"|"+num_bed+"|"+home_size+"\n"
+		write_out=street_address+'|'+zipcode+"|"+zillow_id+"|"+Zestimate+"|"+Home_type+"|"+num_bath+"|"+num_bed+"|"+home_size+"|"+lat+"|"+long_res+"\n"
 		print(str(write_out))
 		data_base.write(write_out)
 		
